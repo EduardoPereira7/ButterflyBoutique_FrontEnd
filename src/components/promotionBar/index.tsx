74210fb58a085226b1c84b5237dcbcb0ccb0683b
Eduardo Pereira <119636074+EduardoPereira7@users.noreply.github.com>
2024-07-24 17:34:38 +0100

    chore: Update colors and typography styles. Add promotionBar

---
+++ src/components/promotionBar/index.tsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+const promotionBar = () => {
+  return (
+    <div className="promotionBarDiv">
+      <p>Get 10% off your first order!</p>
+    </div>
+  );
+};
+export default promotionBar;
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import PromotionBar from "../components/promotionBar/index";
+
+function App() {
+  return (
+    <>
+      <PromotionBar />
+      <div>Hello, World!</div>
+    </>
+  );
+}
+
+export default App;
